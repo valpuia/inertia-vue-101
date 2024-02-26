ae449af60dd30bafcf1861e506136f6809a5ea89
Valpuia <valpuia604@gmail.com>
2024-02-26 14:59:16 +0700

    use controller

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class LanguageController extends Controller
 {
-    //
+    public function update($locale)
+    {
+        app()->setLocale($locale);
+        session()->put('locale', $locale);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,13 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/lang/{locale}', function ($locale) {
-    app()->setLocale($locale);
-    session()->put('locale', $locale);
-})->name('locale');
+Route::post('/lang/{locale}', [LanguageController::class, 'update'])->name('locale');
 
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('home');
