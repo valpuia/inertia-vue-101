a78cde0753728d77f3540cd51fb9d2fa961e48bb
Valpuia <valpuia604@gmail.com>
2024-02-26 13:50:13 +0700

    locale

--- resources/js/Components/LocaleSwitcher.vue
+++ resources/js/Components/LocaleSwitcher.vue
@@ -55,8 +55,8 @@ const switchTheme = (locale) => {
         <div @click="open = !open">
             <span class="inline-flex rounded-md">
                 <button type="button"
-                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-white bg-white dark:bg-gray-600 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                    {{ locale }}
+                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-white bg-white dark:bg-gray-600 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 uppercase">
+                    {{ locale.substr(0, 2) }}
 
                     <LanguageIcon class="ms-2 -me-0.5 h-4 w-4" />
                 </button>
